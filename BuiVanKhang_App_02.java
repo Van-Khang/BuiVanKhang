59796f823c2ab892e53a62d103d14ec08dc74934
Van-Khang <125308490+Van-Khang@users.noreply.github.com>
2023-03-21 08:47:23 +0700

    Bài Tập Chương 2 1,2,3,4,5,6,7,8

---
+++ BuiVanKhang_App_02.java
@@ -0,0 +1,19 @@
+package BuiVanKhang_02;
+
+import java.util.Scanner;
+
+public class BuiVanKhang_App_02 {
+	public static void main(String[] args) {
+		try (Scanner b = new Scanner(System.in)) {
+			System.out.println("Enter a number : ");
+			int c = b.nextInt();
+
+			if(c%2==0) {
+				System.out.println("The number you just entered is an even number");
+			}else {
+				System.out.println("The number you just entered is odd");
+			}
+		}
+	}
+
+}
---
+++ BuiVanKhang_App_07.java
@@ -0,0 +1,22 @@
+package BuiVanKhang_07;
+
+import java.util.Scanner;
+
+public class BuiVanKhang_App_07 {
+	 public static void main(String[] args) {
+	    	System.out.print("Nhap so luong phan tu mang : ");
+			try (Scanner b = new Scanner(System.in)) {
+				int n = b.nextInt();
+				int[] arr = new int[n];
+				int tong = 0;
+				for (int i = 0; i < n; i++) {
+				    System.out.printf("a[%d] = ", i);
+				    arr[i] = b.nextInt();
+				    tong += arr[i];
+				}
+				float d = (float)tong / n;
+				System.out.println("Trung binh cong cua "+n +" so nguyen duong da nhap la : "+d);
+			}
+	    }
+
+}
